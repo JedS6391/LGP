e8e4a0d9537532a9641d65cd88dfc75b0b35bc21
Jed Simson <jed.simson@gmail.com>
2017-04-19 14:27:41 +1200

    Add built-in default value providers.

--- src/main/kotlin/lgp/core/environment/DefaultValueProvider.kt
+++ src/main/kotlin/lgp/core/environment/DefaultValueProvider.kt
@@ -1,5 +1,7 @@
 package lgp.core.environment
 
+import java.util.*
+
 /**
  * An implementation will be able to provide default values to some consumer.
  *
@@ -10,4 +12,42 @@ interface DefaultValueProvider<out TValue> {
      * A value that will be provided to any consumers.
      */
     val value: TValue
+}
+
+object DefaultValueProviders {
+
+    /**
+     * A [DefaultValueProvider] that always returns the value it is initialised with.
+     *
+     * @param T The type of value that this provider gives.
+     * @param value The value the provider should give.
+     * @returns A provider that always gives [value].
+     */
+    @JvmStatic
+    fun <T> constantValueProvider(value: T) = object : DefaultValueProvider<T> {
+        override val value: T
+            get() = value
+    }
+
+    @JvmStatic
+    fun randomDoubleValueProvider() = object : DefaultValueProvider<Double> {
+        private val rg = Random()
+
+        override val value: Double
+            get() = rg.nextDouble()
+    }
+
+    @JvmStatic
+    fun randomGaussianValueProvider() = object : DefaultValueProvider<Double> {
+        private val rg = Random()
+
+        override val value: Double
+            get() = rg.nextGaussian()
+    }
+
+    @JvmStatic
+    fun <T> lambdaValueProvider(func: () -> T) = object : DefaultValueProvider<T> {
+        override val value: T
+            get() = func()
+    }
 }
