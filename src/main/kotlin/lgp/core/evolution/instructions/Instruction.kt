887f2cb40695918e7112a46c2576d72f63c8a7d8
Jed Simson <jed.simson@gmail.com>
2017-04-25 11:41:34 +1200

    Refactor program/instruction modules to include core LGP attributes.

--- src/main/kotlin/lgp/core/evolution/instructions/Instruction.kt
+++ src/main/kotlin/lgp/core/evolution/instructions/Instruction.kt
@@ -1,9 +1,14 @@
 package lgp.core.evolution.instructions
 
+import lgp.core.evolution.registers.Register
 import lgp.core.evolution.registers.RegisterSet
 import lgp.core.modules.Module
 
 abstract class Instruction<T> : Module {
+    abstract var destination: Register<T>
+
+    abstract var operands: MutableList<Register<T>>
+
     abstract var operation: Operation<T>
 
     abstract fun execute(registers: RegisterSet<T>)
--- src/main/kotlin/lgp/core/evolution/population/Program.kt
+++ src/main/kotlin/lgp/core/evolution/population/Program.kt
@@ -1,9 +1,7 @@
 package lgp.core.evolution.population
 
-import lgp.core.evolution.instructions.BranchOperation
 import lgp.core.evolution.instructions.Instruction
 import lgp.core.evolution.registers.RegisterSet
-import lgp.core.evolution.registers.copy
 import lgp.core.modules.Module
 
 /**
@@ -11,16 +9,14 @@ import lgp.core.modules.Module
  */
 abstract class Program<T>(
         var instructions: MutableList<Instruction<T>>,
-        val registers: RegisterSet<T>
+        val registers: RegisterSet<T>,
+        val outputRegisterIndex: Int     // TODO: Add support for multi-output programs
 ) : Module {
 
     var fitness: Double = 0.0
 
     // Empty to begin
     var effectiveInstructions: MutableList<Instruction<T>> = mutableListOf()
-
-    // TODO: Make this configurable
-    val outputRegisterIdx = 0
 
     abstract fun execute()
 
