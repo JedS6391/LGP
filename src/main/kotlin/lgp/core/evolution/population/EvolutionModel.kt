bbb564173be15ccce0c1d478d68a96c645943bb0
Jed Simson <jed.simson@gmail.com>
2017-05-03 14:37:40 +1200

    Add ability for model to return statistics and provide a built-in models module.

--- src/main/kotlin/lgp/core/evolution/population/EvolutionModel.kt
+++ src/main/kotlin/lgp/core/evolution/population/EvolutionModel.kt
@@ -4,12 +4,26 @@ import lgp.core.environment.Environment
 import lgp.core.modules.Module
 
 /**
+ * A container for any statistics from evolution.
+ *
+ * The interface simply maps a string (some description of the statistic) to a value of any type.
+ *
+ * @property data A mapping of a description to some statistic.
+ */
+data class EvolutionStatistics(val data: Map<String, Any>)
+
+/**
  * The best individual and final population from the result of evolution.
  *
  * @property best The best program at the end of the evolution process.
  * @property individuals The population at the end of the evolution process.
+ * @property statistics Any statistics from evolution. It is expected to be on a per generation basis.
  */
-data class EvolutionResult<T>(val best: Program<T>, val individuals: List<Program<T>>)
+data class EvolutionResult<T>(
+        val best: Program<T>,
+        val individuals: List<Program<T>>,
+        val statistics: List<EvolutionStatistics>
+)
 
 /**
  * A model that can be used to perform evolution within in a specific environment.
@@ -23,4 +37,6 @@ abstract class EvolutionModel<TProgram>(val environment: Environment<TProgram>) 
      * Starts the process of evolution using this model.
      */
     abstract fun evolve(): EvolutionResult<TProgram>
+
+    abstract fun copy(): EvolutionModel<TProgram>
 }
