2f68fe6456e568f1f2c4653c29cf4535c95d0352
Jed Simson <jed.simson@gmail.com>
2017-05-12 11:49:10 +1200

    Refactor simple function problem to adhere to the new problem definition format.

--- src/main/kotlin/lgp/examples/SimpleFunction.kt
+++ src/main/kotlin/lgp/examples/SimpleFunction.kt
@@ -1,11 +1,13 @@
 package lgp.examples
 
 import lgp.core.environment.*
-import lgp.core.environment.config.JsonConfigLoader
+import lgp.core.environment.config.Config
+import lgp.core.environment.config.ConfigLoader
 import lgp.core.environment.constants.GenericConstantLoader
 import lgp.core.environment.dataset.*
 import lgp.core.environment.operations.DefaultOperationLoader
-import lgp.core.evolution.Runners
+import lgp.core.evolution.*
+import lgp.core.evolution.fitness.FitnessFunction
 import lgp.core.evolution.fitness.FitnessFunctions
 import lgp.core.evolution.population.*
 import lgp.core.modules.ModuleInformation
@@ -14,144 +16,182 @@ import lgp.lib.BaseProgram
 import lgp.lib.BaseProgramGenerator
 import lgp.lib.BaseProgramSimplifier
 
-/**
+/*
  * An example of setting up an environment to use LGP to find programs for the function `x^2 + 2x + 2`.
  *
  * This example serves as a good way to learn how to use the system and to ensure that everything
  * is working correctly, as some percentage of the time, perfect individuals should be found.
  */
-class SimpleFunction {
-    companion object Main {
-        // Locations of configuration and data set files.
-        private val configFilename = this::class.java.classLoader.getResource("simple_function_env.json").file
 
-        @JvmStatic fun main(args: Array<String>) {
-            // Load up configuration information from the JSON file
-            val configLoader = JsonConfigLoader(
-                    filename = configFilename
+// A solution for this problem consists of the problem's name and a result from
+// running the problem with a `Runner` impl.
+data class SimpleFunctionSolution(
+        override val problem: String,
+        val result: RunResult<Double>
+) : Solution<Double>
+
+// Define the problem and the necessary components to solve it.
+class SimpleFunctionProblem : Problem<Double>() {
+    override val name = "Simple Quadratic."
+
+    override val description = Description("f(x) = x^2 + 2x + 2\n\trange = [-10:10:0.5]")
+
+    override val configLoader = object : ConfigLoader {
+        override val information = ModuleInformation("Overrides default config for this problem.")
+
+        override fun load(): Config {
+            val config = Config()
+
+            config.initialMinimumProgramLength = 10
+            config.initialMaximumProgramLength = 30
+            config.minimumProgramLength = 10
+            config.maximumProgramLength = 200
+            config.operations = listOf(
+                    "lgp.lib.operations.Addition",
+                    "lgp.lib.operations.Subtraction",
+                    "lgp.lib.operations.Multiplication"
             )
+            config.constantsRate = 0.5
+            config.constants = listOf("0.0", "1.0", "2.0")
+            config.numCalculationRegisters = 4
+            config.populationSize = 500
+            config.generations = 1000
+            config.inputAttributesLowIndex = 0
+            config.inputAttributesHighIndex = 0
+            config.classAttributeIndex = 1
+            config.microMutationRate = 0.4
+            config.macroMutationRate = 0.6
+            config.numOffspring = 20
 
-            val config = configLoader.load()
+            return config
+        }
+    }
 
-            // Automatically generate a dataset.
-            val datasetLoader = object : DatasetLoader<Double> {
-                // x^2 + 2x + 2
-                val func = { x: Double -> (x * x) + (2 * x) + 2 }
-                val gen = SequenceGenerator()
+    private val config = this.configLoader.load()
 
-                override val information = ModuleInformation("Generates instances in the range [-10:10:0.5].")
+    override val constantLoader = GenericConstantLoader(
+            constants = config.constants,
+            parseFunction = String::toDouble
+    )
 
-                override fun load(): Dataset<Double> {
-                    val xs = gen.generate(-10.0, 10.0, 0.5, inclusive = true).map { x ->
-                        Attribute(name = "x", value = x)
-                    }
+    override val datasetLoader = object : DatasetLoader<Double> {
+        // x^2 + 2x + 2
+        val func = { x: Double -> (x * x) + (2 * x) + 2 }
+        val gen = SequenceGenerator()
 
-                    val ys = xs.map { x ->
-                        val y = this.func(x.value)
-                        Attribute(name = "y", value = y)
-                    }
+        override val information = ModuleInformation("Generates instances in the range [-10:10:0.5].")
 
-                    val instances = xs.zip(ys).map { (x, y) ->
-                        Instance(listOf(x, y))
-                    }
-
-                    return Dataset(instances.toList())
-                }
+        override fun load(): Dataset<Double> {
+            val xs = gen.generate(-10.0, 10.0, 0.5, inclusive = true).map { x ->
+                Attribute(name = "x", value = x)
             }
 
-            // Set up a loader for loading the operations we want to use (specified in the configuration file)
-            val operationLoader = DefaultOperationLoader<Double>(
-                    operationNames = config.operations
+            val ys = xs.map { x ->
+                val y = this.func(x.value)
+                Attribute(name = "y", value = y)
+            }
+
+            val instances = xs.zip(ys).map { (x, y) ->
+                Instance(listOf(x, y))
+            }
+
+            return Dataset(instances.toList())
+        }
+    }
+
+    override val operationLoader = DefaultOperationLoader<Double>(
+            operationNames = config.operations
+    )
+
+    override val defaultValueProvider = DefaultValueProviders.constantValueProvider(1.0)
+
+    override val fitnessFunction: FitnessFunction<Double> = FitnessFunctions.MSE()
+
+    override val registeredModules = ModuleContainer(
+            modules = mutableMapOf(
+                    CoreModuleType.InstructionGenerator to {
+                        BaseInstructionGenerator(environment)
+                    },
+                    CoreModuleType.ProgramGenerator to {
+                        BaseProgramGenerator(environment, sentinelTrueValue = 1.0)
+                    },
+                    CoreModuleType.SelectionOperator to {
+                        TournamentSelection(environment, tournamentSize = 2)
+                    },
+                    CoreModuleType.RecombinationOperator to {
+                        LinearCrossover(
+                                environment,
+                                maximumSegmentLength = 6,
+                                maximumCrossoverDistance = 5,
+                                maximumSegmentLengthDifference = 3
+                        )
+                    },
+                    CoreModuleType.MacroMutationOperator to {
+                        MacroMutationOperator(
+                                environment,
+                                insertionRate = 0.67,
+                                deletionRate = 0.33
+                        )
+                    },
+                    CoreModuleType.MicroMutationOperator to {
+                        MicroMutationOperator(
+                                environment,
+                                registerMutationRate = 0.5,
+                                operatorMutationRate = 0.5,
+                                // Use identity func. since the probabilities
+                                // of other micro mutations mean that we aren't
+                                // modifying constants.
+                                constantMutationFunc = { v -> v }
+                        )
+                    }
             )
+    )
 
-            // Set up a loader for the constant values (specified in the configuration file)
-            val constantLoader = GenericConstantLoader<Double>(
-                    constants = config.constants,
-                    parseFunction = String::toDouble
-            )
+    override fun initialiseEnvironment() {
+        this.environment = Environment(
+                this.configLoader,
+                this.constantLoader,
+                this.datasetLoader,
+                this.operationLoader,
+                this.defaultValueProvider,
+                this.fitnessFunction
+        )
 
-            // Fill calculation registers with the value 1.0
-            val defaultValueProvider = DefaultValueProviders.constantValueProvider<Double>(1.0)
+        this.environment.registerModules(this.registeredModules)
+    }
 
-            // Use mean-squared error function for fitness evaluations.
-            val mse = FitnessFunctions.MSE()
+    override fun initialiseModel() {
+        this.model = Models.SteadyState(this.environment)
+    }
 
-            // Create a new environment with these components.
-            val environment = Environment<Double>(
-                    configLoader,
-                    constantLoader,
-                    datasetLoader,
-                    operationLoader,
-                    defaultValueProvider,
-                    fitnessFunction = mse
-            )
-
-            // Set up registered modules
-            val container = ModuleContainer(
-                    modules = mutableMapOf(
-                            CoreModuleType.InstructionGenerator to {
-                                BaseInstructionGenerator(environment)
-                            },
-                            CoreModuleType.ProgramGenerator to {
-                                BaseProgramGenerator(environment, sentinelTrueValue = 1.0)
-                            },
-                            CoreModuleType.SelectionOperator to {
-                                TournamentSelection(environment, tournamentSize = 2)
-                            },
-                            CoreModuleType.RecombinationOperator to {
-                                LinearCrossover(
-                                        environment,
-                                        maximumSegmentLength = 6,
-                                        maximumCrossoverDistance = 5,
-                                        maximumSegmentLengthDifference = 3
-                                )
-                            },
-                            CoreModuleType.MacroMutationOperator to {
-                                MacroMutationOperator(
-                                        environment,
-                                        insertionRate = 0.67,
-                                        deletionRate = 0.33
-                                )
-                            },
-                            CoreModuleType.MicroMutationOperator to {
-                                MicroMutationOperator(
-                                        environment,
-                                        registerMutationRate = 0.5,
-                                        operatorMutationRate = 0.5,
-                                        // Use identity func. since the probabilities
-                                        // of other micro mutations mean that we aren't
-                                        // modifying constants.
-                                        constantMutationFunc = { v -> v }
-                                )
-                            }
-                    )
-            )
-
-            // Alternatively...
-            /*
-                environment.registerModule(
-                    CoreModuleType.InstructionGenerator,
-                    { BaseInstructionGenerator(environment) }
-                )
-                environment.registerModule(
-                    CoreModuleType.ProgramGenerator,
-                    { BaseProgramGenerator(environment, sentinelTrueValue = 1.0) }
-                )
-                ... For each module
-             */
-
-            environment.registerModules(container)
-
-            // Find the best individual with these parameters.
-            val model = Models.SteadyState(environment)
-            
+    override fun solve(): SimpleFunctionSolution {
+        try {
             val runner = Runners.DistributedRunner(environment, model, runs = 10)
             val result = runner.run()
+
+            return SimpleFunctionSolution(this.name, result)
+        } catch (ex: UninitializedPropertyAccessException) {
+            // The initialisation routines haven't been run.
+            throw ProblemNotInitialisedException(
+                    "The initialisation routines for this problem must be run before it can be solved."
+            )
+        }
+    }
+}
+
+class SimpleFunction {
+    companion object Main {
+        @JvmStatic fun main(args: Array<String>) {
+            // Create a new problem instance, initialise it, and then solve it.
+            val problem = SimpleFunctionProblem()
+            problem.initialiseEnvironment()
+            problem.initialiseModel()
+            val solution = problem.solve()
             val simplifier = BaseProgramSimplifier<Double>()
 
             println("Results:")
 
-            result.evaluations.forEachIndexed { run, res ->
+            solution.result.evaluations.forEachIndexed { run, res ->
                 println("Run ${run + 1} (best fitness = ${res.best.fitness})")
                 println(simplifier.simplify(res.best as BaseProgram<Double>))
 
@@ -163,7 +203,10 @@ class SimpleFunction {
                 println("")
             }
 
-            val avgBestFitness = result.evaluations.map { eval -> eval.best.fitness }.sum() / result.evaluations.size
+            val avgBestFitness = solution.result.evaluations.map { eval ->
+                eval.best.fitness
+            }.sum() / solution.result.evaluations.size
+
             println("Average best fitness (over 10 runs): $avgBestFitness")
         }
     }
