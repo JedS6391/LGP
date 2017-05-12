1fad8119cec383ad303c3a9808f35e47a0265389
Jed Simson <jed.simson@gmail.com>
2017-05-12 14:52:05 +1200

    Update source documentation.

--- src/main/kotlin/lgp/core/environment/dataset/ValueGenerator.kt
+++ src/main/kotlin/lgp/core/environment/dataset/ValueGenerator.kt
@@ -3,8 +3,31 @@ package lgp.core.environment.dataset
 import java.util.*
 import kotlin.coroutines.experimental.buildSequence
 
+/**
+ * Can generate a sequence of values evenly spread between a range.
+ *
+ * This generator is useful for generating values between two bounds
+ * with a certain interval between them (e.g. [-5, 5] in 0.5 steps.
+ */
 class SequenceGenerator {
-    fun generate(start: Double, end: Double, step: Double, inclusive: Boolean = false): Sequence<Double> = buildSequence {
+
+    /**
+     * Generates a sequence of values between [start] and [end], with an interval of [step] between each value.
+     *
+     * If [inclusive] is true, then the range will include the end point (i.e. [start, end]); otherwise,
+     * the range will be exclusive (i.e. [start, end))
+     *
+     * @param start The starting point of the range of values.
+     * @param end The ending point of the range of values.
+     * @param step The interval between each value in the sequence.
+     * @param inclusive When true, the range will include [end].
+     */
+    fun generate(
+            start: Double,
+            end: Double,
+            step: Double,
+            inclusive: Boolean = false
+    ): Sequence<Double> = buildSequence {
         var x = start
 
         while (x <= end) {
@@ -18,7 +41,18 @@ class SequenceGenerator {
     }
 }
 
+/**
+ * Can generate a number of uniformly distributed values.
+ */
 class UniformlyDistributedGenerator {
+
+    /**
+     * Generates [n] values that are uniformly distributed between [start] and [end].
+     *
+     * @param n Number of values to generate.
+     * @param start Lower bound on the range of values.
+     * @param end Upper bound on the range of values.
+     */
     fun generate(n: Int, start: Double, end: Double): Sequence<Double> = buildSequence {
         val random = Random()
 
--- src/main/kotlin/lgp/core/evolution/Problem.kt
+++ src/main/kotlin/lgp/core/evolution/Problem.kt
@@ -12,29 +12,111 @@ import lgp.core.evolution.population.EvolutionModel
 
 data class Description(val description: String)
 
+/**
+ * Represents a solution to a problem.
+ *
+ * The interface is left open so that when defining one's problem, it is straight-forward
+ * to define the format of the solution, as it will probably depend on the use case
+ * on what the solution should actually contain.
+ */
 interface Solution<T> {
+
+    /**
+     * The name of the problem this solution is for.
+     */
     val problem: String
 }
 
+/**
+ * Exception given when a problem is attempted to be solved when it hasn't
+ * been completely initialised.
+ */
 class ProblemNotInitialisedException(message: String) : Exception(message)
 
+/**
+ * Defines a problem and the components that should be used to solve that problem.
+ */
 abstract class Problem<T> {
+
+    /**
+     * A name for this problem.
+     */
     abstract val name: String
+
+    /**
+     * A brief description of the problem (e.g. it's function, feature range, etc.)
+     */
     abstract val description: Description
 
+    /**
+     * A component that can provide configuration for the problem.
+     */
     abstract val configLoader: ConfigLoader
+
+    /**
+     * A component that can provide constants for the problem.
+     */
     abstract val constantLoader: ConstantLoader<T>
+
+    /**
+     * A component that can provide a dataset for the problem.
+     */
     abstract val datasetLoader: DatasetLoader<T>
+
+    /**
+     * A component that can provide operations for the problem.
+     */
     abstract val operationLoader: OperationLoader<T>
+
+    /**
+     * A component that provides default values to the register set.
+     */
     abstract val defaultValueProvider: DefaultValueProvider<T>
+
+    /**
+     * A fitness metric to be used for this problem.
+     */
     abstract val fitnessFunction: FitnessFunction<T>
 
+    /**
+     * A collection of modules that should be registered with the environment.
+     */
     abstract val registeredModules: ModuleContainer
 
+    /**
+     * An environment built up of the components this problem uses.
+     *
+     * The environment should be initialised in the [initialiseEnvironment] method.
+     */
     lateinit var environment: Environment<T>
+
+    /**
+     * An evolutionary model that should be used to solve this problem.
+     *
+     * The model should be initialised in the [initialiseModel] method.
+     */
     lateinit var model: EvolutionModel<T>
 
+    // TODO: Figure out a way to call initialisation methods automatically.
+
+    /**
+     * Initialises the environment for this problem.
+     *
+     * **NB:** This method must be called before [solve].
+     */
     abstract fun initialiseEnvironment()
+
+    /**
+     * Initialises the model for this problem.
+     *
+     * **NB:** This method must be called before [solve].
+     */
     abstract fun initialiseModel()
+
+    /**
+     * Solves the problem in some way and gives a solution.
+     *
+     * @return A solution for the problem.
+     */
     abstract fun solve(): Solution<T>
 }
