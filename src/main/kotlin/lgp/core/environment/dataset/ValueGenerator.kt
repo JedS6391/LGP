918242c3491fdfd0d528d4d38525bf9f026e8ac0
Jed Simson <jed.simson@gmail.com>
2017-05-12 13:24:11 +1200

    Refactor sinpoly example.

--- src/main/kotlin/lgp/core/environment/dataset/ValueGenerator.kt
+++ src/main/kotlin/lgp/core/environment/dataset/ValueGenerator.kt
@@ -1,5 +1,6 @@
 package lgp.core.environment.dataset
 
+import java.util.*
 import kotlin.coroutines.experimental.buildSequence
 
 class SequenceGenerator {
@@ -14,5 +15,18 @@ class SequenceGenerator {
 
         if (x - step < end && inclusive)
             yield(end)
+    }
+}
+
+class UniformlyDistributedGenerator {
+    fun generate(n: Int, start: Double, end: Double): Sequence<Double> = buildSequence {
+        val random = Random()
+
+        (0..n).map {
+            val r = random.nextDouble()
+
+            // Scaled to range
+            yield(r * (end - start) + start)
+        }
     }
 }
--- src/main/kotlin/lgp/examples/SinPoly.kt
+++ src/main/kotlin/lgp/examples/SinPoly.kt
@@ -1,109 +1,188 @@
 package lgp.examples
 
 import lgp.core.environment.*
+import lgp.core.environment.config.Config
+import lgp.core.environment.config.ConfigLoader
 import lgp.core.environment.config.JsonConfigLoader
 import lgp.core.environment.constants.GenericConstantLoader
-import lgp.core.environment.dataset.CsvDatasetLoader
+import lgp.core.environment.dataset.*
 import lgp.core.environment.operations.DefaultOperationLoader
-import lgp.core.evolution.Runners
+import lgp.core.evolution.*
+import lgp.core.evolution.fitness.FitnessFunction
 import lgp.core.evolution.fitness.FitnessFunctions
 import lgp.core.evolution.population.*
+import lgp.core.modules.ModuleInformation
 import lgp.lib.BaseInstructionGenerator
 import lgp.lib.BaseProgram
 import lgp.lib.BaseProgramGenerator
 import lgp.lib.BaseProgramSimplifier
 import java.util.*
 
-class SinPoly {
-    companion object Main {
-        // Locations of configuration and data set files.
-        private val configFilename = this::class.java.classLoader.getResource("sinpoly_env.json").file
-        private val datasetFilename = this::class.java.classLoader.getResource("sinpoly.csv").file
+data class SinPolySolution(
+        override val problem: String,
+        val result: RunResult<Double>
+) : Solution<Double>
 
-        @JvmStatic fun main(args: Array<String>) {
-            val configLoader = JsonConfigLoader(
-                    filename = configFilename
+class SinPolyProblem : Problem<Double>() {
+    override val name = "SinPoly."
+
+    override val description = Description("f(x) = sin(x) * x + 5\n\trange = Uniform[-5:5]")
+
+    override val configLoader = object : ConfigLoader {
+        override val information = ModuleInformation("Overrides default config for this problem.")
+
+        override fun load(): Config {
+            val config = Config()
+
+            config.initialMinimumProgramLength = 10
+            config.initialMaximumProgramLength = 30
+            config.minimumProgramLength = 10
+            config.maximumProgramLength = 200
+            config.operations = listOf(
+                    "lgp.lib.operations.Addition",
+                    "lgp.lib.operations.Subtraction",
+                    "lgp.lib.operations.Multiplication",
+                    "lgp.lib.operations.Division",
+                    "lgp.lib.operations.Exponent"
             )
-
-            val config = configLoader.load()
-
-            val datasetLoader = CsvDatasetLoader(
-                    filename = datasetFilename,
-                    parseFunction = String::toDouble
+            config.constantsRate = 0.5
+            config.constants = listOf(
+                    "0.0", "1.0", "2.0", "3.0", "4.0", "5.0", "6.0", "7.0", "8.0", "9.0"
             )
+            config.numCalculationRegisters = 4
+            config.populationSize = 1000
+            config.generations = 500
+            config.inputAttributesLowIndex = 0
+            config.inputAttributesHighIndex = 0
+            config.classAttributeIndex = 1
+            config.microMutationRate = 0.25
+            config.macroMutationRate = 0.75
+            config.branchInitialisationRate = 0.2
+            config.numOffspring = 10
 
-            val operationLoader = DefaultOperationLoader<Double>(
-                    operationNames = config.operations
+            return config
+        }
+    }
+
+    private val config = this.configLoader.load()
+
+    override val constantLoader = GenericConstantLoader(
+            constants = config.constants,
+            parseFunction = String::toDouble
+    )
+
+    override val datasetLoader = object : DatasetLoader<Double> {
+        // f(x) = sin(x) * x + 5
+        val func = { x: Double -> Math.sin(x) * x + 5 }
+        val gen = UniformlyDistributedGenerator()
+
+        override val information = ModuleInformation("Generates uniformly distributed instances in the range [-5:5].")
+
+        override fun load(): Dataset<Double> {
+            val table = gen.generate(100, -5.0, 5.0).map { v ->
+                val x = Attribute(name = "x", value = v)
+                val y = Attribute(name = "y", value = this.func(x.value))
+
+                Pair(x, y)
+            }
+
+            val instances = table.map { (x, y) ->
+                Instance(listOf(x, y))
+            }
+
+            return Dataset(instances.toList())
+        }
+    }
+
+    override val operationLoader = DefaultOperationLoader<Double>(
+            operationNames = config.operations
+    )
+
+    override val defaultValueProvider = DefaultValueProviders.constantValueProvider(1.0)
+
+    override val fitnessFunction: FitnessFunction<Double> = FitnessFunctions.SSE()
+
+    override val registeredModules = ModuleContainer(
+            modules = mutableMapOf(
+                    CoreModuleType.InstructionGenerator to {
+                        BaseInstructionGenerator(environment)
+                    },
+                    CoreModuleType.ProgramGenerator to {
+                        BaseProgramGenerator(environment, sentinelTrueValue = 1.0)
+                    },
+                    CoreModuleType.SelectionOperator to {
+                        TournamentSelection(environment, tournamentSize = 4)
+                    },
+                    CoreModuleType.RecombinationOperator to {
+                        LinearCrossover(
+                                environment,
+                                maximumSegmentLength = 6,
+                                maximumCrossoverDistance = 5,
+                                maximumSegmentLengthDifference = 3
+                        )
+                    },
+                    CoreModuleType.MacroMutationOperator to {
+                        MacroMutationOperator(
+                                environment,
+                                insertionRate = 0.67,
+                                deletionRate = 0.33
+                        )
+                    },
+                    CoreModuleType.MicroMutationOperator to {
+                        MicroMutationOperator(
+                                environment,
+                                registerMutationRate = 0.3,
+                                operatorMutationRate = 0.4,
+                                constantMutationFunc = { v -> v + (Random().nextGaussian() * 1) }
+                        )
+                    }
             )
+    )
 
-            val constantLoader = GenericConstantLoader(
-                    constants = config.constants,
-                    parseFunction = String::toDouble
-            )
+    override fun initialiseEnvironment() {
+        this.environment = Environment(
+                this.configLoader,
+                this.constantLoader,
+                this.datasetLoader,
+                this.operationLoader,
+                this.defaultValueProvider,
+                this.fitnessFunction
+        )
 
-            val defaultValueProvider = DefaultValueProviders.constantValueProvider(1.0)
+        this.environment.registerModules(this.registeredModules)
+    }
 
-            val sse = FitnessFunctions.SSE()
+    override fun initialiseModel() {
+        this.model = Models.SteadyState(this.environment)
+    }
 
-            val environment = Environment(
-                    configLoader,
-                    constantLoader,
-                    datasetLoader,
-                    operationLoader,
-                    defaultValueProvider,
-                    fitnessFunction = sse
-            )
-
-            // Set up registered modules
-            val container = ModuleContainer(
-                    modules = mutableMapOf(
-                            CoreModuleType.InstructionGenerator to {
-                                BaseInstructionGenerator(environment)
-                            },
-                            CoreModuleType.ProgramGenerator to {
-                                BaseProgramGenerator(environment, sentinelTrueValue = 1.0)
-                            },
-                            CoreModuleType.SelectionOperator to {
-                                TournamentSelection(environment, tournamentSize = 4)
-                            },
-                            CoreModuleType.RecombinationOperator to {
-                                LinearCrossover(
-                                        environment,
-                                        maximumSegmentLength = 6,
-                                        maximumCrossoverDistance = 5,
-                                        maximumSegmentLengthDifference = 3
-                                )
-                            },
-                            CoreModuleType.MacroMutationOperator to {
-                                MacroMutationOperator(
-                                        environment,
-                                        insertionRate = 0.67,
-                                        deletionRate = 0.33
-                                )
-                            },
-                            CoreModuleType.MicroMutationOperator to {
-                                MicroMutationOperator(
-                                        environment,
-                                        registerMutationRate = 0.3,
-                                        operatorMutationRate = 0.4,
-                                        constantMutationFunc = { v -> v + (Random().nextGaussian() * 1) }
-                                )
-                            }
-                    )
-            )
-
-            environment.registerModules(container)
-
-            // Find the best individual with these parameters.
-            val model = Models.SteadyState(environment)
-
+    override fun solve(): SinPolySolution {
+        try {
             val runner = Runners.DistributedRunner(environment, model, runs = 10)
             val result = runner.run()
+
+            return SinPolySolution(this.name, result)
+        } catch (ex: UninitializedPropertyAccessException) {
+            // The initialisation routines haven't been run.
+            throw ProblemNotInitialisedException(
+                    "The initialisation routines for this problem must be run before it can be solved."
+            )
+        }
+    }
+}
+
+class SinPoly {
+    companion object Main {
+        @JvmStatic fun main(args: Array<String>) {
+            val problem = SinPolyProblem()
+            problem.initialiseEnvironment()
+            problem.initialiseModel()
+            val solution = problem.solve()
             val simplifier = BaseProgramSimplifier<Double>()
 
             println("Results:")
 
-            result.evaluations.forEachIndexed { run, res ->
+            solution.result.evaluations.forEachIndexed { run, res ->
                 println("Run ${run + 1} (best fitness = ${res.best.fitness})")
                 println(simplifier.simplify(res.best as BaseProgram<Double>))
 
@@ -115,7 +194,9 @@ class SinPoly {
                 println("")
             }
 
-            val avgBestFitness = result.evaluations.map { eval -> eval.best.fitness }.sum() / result.evaluations.size
+            val avgBestFitness = solution.result.evaluations.map { (best) ->
+                best.fitness
+            }.sum() / solution.result.evaluations.size
             println("Average best fitness (over 10 runs): $avgBestFitness")
         }
     }
