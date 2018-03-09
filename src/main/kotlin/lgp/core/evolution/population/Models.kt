10799d1508963c4131ef49af4054285d418bb08f
Jed Simson <jed.simson@gmail.com>
2018-03-10 10:34:03 +1300

    Update RunBasedExportableResult documentation.

--- src/main/kotlin/lgp/core/evolution/population/Models.kt
+++ src/main/kotlin/lgp/core/evolution/population/Models.kt
@@ -11,7 +11,23 @@ import java.util.Random
 import kotlin.concurrent.thread
 import kotlin.streams.toList
 
-class RunBasedExportableResult<T>(val run: Int, val statistics: EvolutionStatistics) : ExportableResult<T> {
+/**
+ * An [ExportableResult] implementation that represents evolution statistics for a particular run.
+ *
+ * Because evolution statistics are collected on a per-generation basis, this type of exportable result
+ * collects data on each generation for each run.
+ *
+ * @param run The run this result relates to.
+ * @param statistics Evolution statistics for a particular generation.
+ */
+class RunBasedExportableResult<T>(
+    val run: Int,
+    private val statistics: EvolutionStatistics
+) : ExportableResult<T> {
+
+    /**
+     * Exports this result as a mapping of statistic names to statistic values.
+     */
     override fun export(): List<Pair<String, String>> {
         val out = mutableListOf(
                 Pair("run", this.run.toString())
