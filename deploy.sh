c0bd95751ddbc93d51601a6636983dd91468f39c
Jed Simson <jed.simson@gmail.com>
2018-03-13 20:31:54 +1300

    Handle overwrites better.

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,14 @@
 
 deployment_type=$1
 branch_name=$2
+now=$(date +%Y-%m-%d.%H:%M:%S)
 
 case "$deployment_type" in
 "feature")
     echo "Renaming build files for feature branch..."
     rm build/libs/LGP-1.1.jar
-    mv build/libs/LGP-core-1.1.jar build/libs/LGP-core-"$branch_name".jar
-    mv build/libs/LGP-examples-1.1.jar build/libs/LGP-examples-"$branch_name".jar
+    mv build/libs/LGP-core-1.1.jar build/libs/LGP-core-"$branch_name"-"$now".jar
+    mv build/libs/LGP-examples-1.1.jar build/libs/LGP-examples-"$branch_name"-"$now".jar
     ls build/libs
     ;;
 "release")
