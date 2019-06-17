8783100ceb2e90c9acd17b290019faea2d487166
Jed Simson <jed.simson@gmail.com>
2019-06-18 11:35:01 +1200

    Run tests during build.

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,7 +2,13 @@
 
 chmod u+x gradlew
 
+echo "Compiling core JAR..."
+
 ./gradlew coreJar --no-daemon --stacktrace --console=plain
+
+echo "Running tests..."
+
+./gradlew test --no-daemon --info --console=plain
 
 echo "real branch:"
 echo $REAL_BRANCH
